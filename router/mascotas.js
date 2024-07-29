ca21a517b356875af98adc53fe59009c896fc2ee
Damian Garcia <damiangt13@gmail.com>
2024-07-29 07:51:54 -0500

    vista de mascotas dinamica

--- router/mascotas.js
+++ router/mascotas.js
@@ -2,7 +2,12 @@ const express = require('express')
 const router = express.Router()
 
 router.get('/', (req, res) => {
-    res.render('mascotas', {titulo: "Página de mascotas"});
+    res.render('mascotas', {arrayMascotas: [
+        {id: 'mas001', nombre: 'Marly', descripcion: 'Gato criollo'},
+        {id: 'mas002', nombre: 'Manchas', descripcion: 'Gato de raza'},
+        {id: 'mas003', nombre: 'Risas', descripcion: 'Perro criollo'},
+        {id: 'mas004', nombre: 'Axel', descripcion: 'Perro criollo'},
+    ]});
 })
 
 module.exports = router;
